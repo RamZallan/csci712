f7a45c5f7e6a6171af98a256bb8c0924af282ddc
Ram Zallan <ramzallan@gmail.com>
2019-10-18 14:06:57 -0400

    Updating project

--- project/assets/js/animation.js
+++ project/assets/js/animation.js
@@ -1,3 +1,59 @@
+
+const SHAPES = [
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.ConeGeometry(1, 1.5, 8),
+    'torus',
+    'teapot', // lat < -30,
+    'cylinder',
+    'cone',
+];
+
+const TRANSLATION_TYPES = [
+    'none',
+    'up-and-down',
+    'left-and-right',
+    'all-around',
+];
+
+const ROTATION_AXES = [
+    'x',
+    'y',
+    'z',
+];
+
+const ROTATION_TYPES = [
+    'none',
+    'constant',
+    'back-and-forth',
+];
+
+const SCALE_TYPES = [
+     'pulsating',
+     'none',
+];
+
+const TRANSLATION_BOUND = 1;
+
+function getOptions(info) {
+    const options = {};
+
+    if (!info.position) {
+        options.primaryShape = SHAPES[3];
+    } else if (info.position.latitude > 30) {
+        options.primaryShape = SHAPES[0];
+    } else if (info.position.latitude > -30) {
+        options.primaryShape = SHAPES[1];
+    } else {
+        options.primaryShape = SHAPES[2];
+    }
+
+    options.primaryColor = `hsl(${new Date().getTime() % 255}, 50%, 50%)`;
+
+    options.primaryTranslation = info.width < info.height ? TRANSLATION_TYPES[1] : TRANSLATION_TYPES[2];
+
+    return options;
+}
+
 function getUserLocation() {
     if (navigator.geolocation) {
         return new Promise(
@@ -23,21 +79,67 @@ function getHash(data) {
     });
 }
 
+function animate(options) {
+    const width = document.body.clientWidth;
+    const height = document.body.clientHeight;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    document.body.appendChild(renderer.domElement);
+
+    const scene = new THREE.Scene();
+
+    const primaryGeometry = options.primaryShape;
+    const primaryMaterial = new THREE.MeshPhongMaterial({ color: options.primaryColor });
+    const primary = new THREE.Mesh(primaryGeometry, primaryMaterial);
+    scene.add(primary);
+
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.y = 2;
+    camera.position.z = 6;
+    camera.lookAt(primary.position);
+
+    scene.add(camera);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(0, 5, 10);
+    scene.add(pointLight);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.25);
+    scene.add(ambientLight);
+
+    const translationAxis = options.primaryTranslation == 'up-and-down' ? 'y' : 'x';
+    let translationCoeff = 1;
+
+    function render(options) {
+        requestAnimationFrame(render);
+        primary.position[translationAxis] += translationCoeff * 0.05;
+        if (Math.abs(primary.position[translationAxis]) >= TRANSLATION_BOUND) {
+            translationCoeff *= -1;
+        }
+        renderer.render(scene, camera);
+    }
+
+    render();
+}
+
 function init() {
-    console.log(navigator.userAgent);
     const data = {
         width: $(window).width(),
         height: $(window).height(),
         datetime: new Date().getTime(),
         useragent: navigator.userAgent,
     };
-    let hash;
     getUserLocation()
         .then((position) => {
-            getHash({ ...data, position: position.coords });
+            const options = getOptions({ ...data, position: position.coords });
+            animate(options);
+            console.log(options);
         })
         .catch((error) => {
-            getHash(data);
+            const options = getOptions(data);
+            animate(options);
+            console.log(options);
         });
 }
 
